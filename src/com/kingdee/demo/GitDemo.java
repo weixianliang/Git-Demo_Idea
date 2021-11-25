8a9dae6bcbded6da505de72c783b1515d287d670
weixianliang <1378913383@qq.com>
2021-11-25 23:33:57 +0800

    third commit！

--- src/com/kingdee/demo/GitDemo.java
+++ src/com/kingdee/demo/GitDemo.java
@@ -11,5 +11,6 @@ public class GitDemo {
         System.out.println("Hello Git 1!");
         System.out.println("Hello Git 2!");
         System.out.println("Hello Git 3!");
+        System.out.println("Hello Git Third!");
     }
 }
