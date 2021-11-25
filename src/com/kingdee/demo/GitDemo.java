b03fa1ed4a6e9dd225b7b661a7c3751f77446e22
weixianliang <1378913383@qq.com>
2021-11-25 23:28:19 +0800

    first commit！

---
+++ src/com/kingdee/demo/GitDemo.java
@@ -0,0 +1,15 @@
+package com.kingdee.demo;
+
+/**
+ * Description Todo
+ * Date 2021-11-25 22:12
+ * author hp
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git !");
+
+        System.out.println("Hello Git 2!");
+    }
+}
