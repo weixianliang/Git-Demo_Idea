e3be24d5f77eabd070954cedc8b5db265239a0e4
weixianliang <1378913383@qq.com>
2021-11-25 23:31:17 +0800

    second commit！

--- src/com/kingdee/demo/GitDemo.java
+++ src/com/kingdee/demo/GitDemo.java
@@ -8,8 +8,8 @@ package com.kingdee.demo;
 public class GitDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git !");
-
+        System.out.println("Hello Git 1!");
         System.out.println("Hello Git 2!");
+        System.out.println("Hello Git 3!");
     }
 }
